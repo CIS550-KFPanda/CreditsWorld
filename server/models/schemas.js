ee40f285e7d559bd807a64c139e10b48e1c2e833
Neil Shweky <neilshweky@gmail.com>
2020-04-03 16:18:44 -0400

    working on loader [WIP]

--- server/models/schemas.js
+++ server/models/schemas.js
@@ -1,3 +1,7 @@
+/*
+  scehmas.js: connects the mysql database to be used in other parts of the app
+*/
+
 const moment = require('moment');
 const SHA256 = require('crypto-js/sha256');
 const credentials = require('../credentials.js')
@@ -7,6 +11,8 @@ const connection = mysql.createConnection(credentials);
  
 connection.connect();
 
+//Wrapped a connection.query in a Promise bec they let you group promises together  
+//and utilize .then that simplifies code 
 const query = str => {
   return new Promise(function(resolve, reject) {
     connection.query(str, function (err, rows, fields) {
@@ -15,5 +21,20 @@ const query = str => {
     })
   })
 }
+
+//Example of what our calls would look like if we didn't have this wrapper
+/* 
+connection.query(str, function() {
+  connection.query(str, function() {
+    connection.query(str, function() {
+      connection.query(str, function() {}
+      
+    }
+  }
+})
+db.query(str)
+  .then(data => db.query())
+  .then(data => ...)
+*/
 
 module.exports = { query, connection }
