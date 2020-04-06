9e9e69d095a09469ad4a0c2c667f882e3d0bbb2d
Neil Shweky <neilshweky@gmail.com>
2020-04-06 13:17:59 -0400

    ddls

---
+++ server/ddls.sql
@@ -0,0 +1,51 @@
+CREATE TABLE Songs (
+  id varchar(255),
+  title varchar(255),
+  album varchar(255),
+  label varchar(255),
+  youtube_url varchar(255),
+  song_art_image_thumbnail_url varchar(255),
+  release_date_for_display varchar(255),
+  PRIMARY KEY (id)
+); 
+CREATE TABLE Person (
+  id varchar(255),
+  name varchar(255),
+  image_url varchar(255),
+  url varchar(255),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE Artists(
+  artist_id varchar(255),
+  genius_followers int,
+  genius_iq int,
+  PRIMARY KEY (artist_id),
+  FOREIGN KEY (artist_id) REFERENCES Person(id)
+);
+
+CREATE TABLE Sings(
+  artist_id varchar(255),
+  song_id varchar(255),
+  PRIMARY KEY (artist_id,song_id),
+  FOREIGN KEY (artist_id) REFERENCES Artists(artist_id),
+  FOREIGN KEY (song_id) REFERENCES Songs(id)
+);
+
+CREATE TABLE Crew_in (
+  crew_id varchar(255),
+  song_id varchar(255),
+  type varchar(255),
+  PRIMARY KEY (crew_id, song_id),
+  FOREIGN KEY (crew_id) REFERENCES Person(id),
+  FOREIGN KEY (song_id) REFERENCES Songs(id)
+)
+
+CREATE TABLE Entries (
+  song_id varchar(255),
+  position int,
+  date int,
+  streams int,
+  PRIMARY KEY (song_id, date),
+  FOREIGN KEY (song_id) REFERENCES Songs(id)
+)
