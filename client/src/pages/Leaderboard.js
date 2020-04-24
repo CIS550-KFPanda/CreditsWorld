62bc321a877c0672918d09d1ee3a5f89a2885385
Will Schwalbe <williamschwalbe@gmail.com>
2020-04-23 23:55:24 -0400

    added basic leadboard

--- client/src/pages/Leaderboard.js
+++ client/src/pages/Leaderboard.js
@@ -2,6 +2,10 @@ import React from 'react';
 import '../style/Landing.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import PageNavbar from '../components/PageNavbar';
+import Table from 'react-bootstrap/Table'
+import Image from 'react-bootstrap/Image'
+import Dropdown from 'react-bootstrap/Dropdown'
+
 
 
 export default class Leaderboard extends React.Component {
@@ -17,6 +21,7 @@ export default class Leaderboard extends React.Component {
 
   // React function that is called when the page load.
   componentDidMount() {
+
   }
 
   render() {    
@@ -24,6 +29,42 @@ export default class Leaderboard extends React.Component {
       <div className="">
         <PageNavbar active="search-song" />
         <h1> Leaderboard</h1>
+        <Dropdown>
+  <Dropdown.Toggle variant="success" id="dropdown-basic">
+  Top Artists
+  </Dropdown.Toggle>
+
+  <Dropdown.Menu>
+    <Dropdown.Item href="#/action-1">Top Artists</Dropdown.Item>
+    <Dropdown.Item href="#/action-2">Top Songs</Dropdown.Item>
+    <Dropdown.Item href="#/action-3">Top Producers</Dropdown.Item>
+    <Dropdown.Item href="#/action-3">Top Songwriters</Dropdown.Item>
+
+  </Dropdown.Menu>
+</Dropdown>
+        <Table striped bordered hover>
+  <thead>
+    <tr>
+      <th>Rank</th>
+      <th>Artist Name</th>
+      <th>Top Song</th>
+      <th>Username</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td><a href= "/search-person" >
+        <Image src = "https://images.genius.com/b8a71aef947716b9e24dcbea07fd23d1.800x800x1.jpg"  height="50" roundedCircle/> Justin Bibebr
+        </a></td>
+      <td><a href= "/search-song" >
+           What do you mean
+        </a></td>
+      <td>@justinBieber</td>
+    </tr>
+
+  </tbody>
+</Table>
       </div>
     );
   }
